c081548ca024b36d90281fc51b6c80e96fc62237
saikumarvarri <varrisaikumar2000@gmail.com>
2024-06-05 18:21:08 +0530

    Create littlefashion.sh

---
+++ littlefashion.sh
@@ -0,0 +1,12 @@
+URL=https://www.tooplate.com/zip-templates/2127_little_fashion.zip
+     FILE=2127_little_fashion
+     sudo yum install httpd unzip wget -y
+     sudo systemctl start httpd
+      mkdir -p /home/centos/webfiles
+      cd /home/centos/webfiles
+      wget $URL
+      sudo unzip $FILE.zip
+      sudo rm -rf /var/www/html/*
+      sudo cp -r $FILE/* /var/www/html/
+      sudo rm -rf /home/centos/webfiles
+      sudo systemctl restart httpd
